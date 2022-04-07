ac85f6aa42469febf7275e49580f69cc416b2379
jakeseo <n00nietzsche@gmail.com>
2022-04-07 20:31:25 +0900

    AppTest 제거

--- app/src/test/java/com/codesoom/demo/AppTest.java
+++ app/src/test/java/com/codesoom/demo/AppTest.java
@@ -7,10 +7,4 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-<<<<<<< HEAD
-    @Test void appHasAGreeting() {
-
-    }
-=======
->>>>>>> b0dded9cd66afdbe32c4e3e65eeafe84a67ca1d7
 }
