c988691d48cd445a094cd701d572e5ed714ece9c
n00nietzsche <n00nietzsche@gmail.com>
2022-04-18 01:00:57 +0900

    질문 추가

--- app/src/test/java/com/codesoom/demo/Utf8WebMvcTest.java
+++ app/src/test/java/com/codesoom/demo/Utf8WebMvcTest.java
@@ -15,6 +15,11 @@ import java.nio.charset.StandardCharsets;
 @WebMvcTest
 @Import(Utf8WebMvcTest.Config.class)
 public @interface Utf8WebMvcTest {
+    /**
+     * 이게 왜 적용이 잘 됐는지에 대해 알아보기
+     * 1. annotation value 어떻게 쓰는 건지?
+     * 2. `@AliasFor` 의 역할은 무엇인지?
+     */
     @AliasFor("controllers")
     Class<?>[] value() default {};
 
