82ebf7f04c9d3cda718165ba5f52b3dd22ab146a
n00nietzsche <n00nietzsche@gmail.com>
2022-05-03 23:04:02 +0900

    스프링 시큐리티 1강 끝

--- app/src/main/java/com/codesoom/demo/filters/AuthenticationErrorFilter.java
+++ app/src/main/java/com/codesoom/demo/filters/AuthenticationErrorFilter.java
@@ -4,8 +4,6 @@ import com.codesoom.demo.exceptions.InvalidAccessTokenException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
